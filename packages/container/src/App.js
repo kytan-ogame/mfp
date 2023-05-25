969d5c863aefeccc13c6482eb392f98bb0259ea8
julien.valery <julien.valery@manomano.com>
2023-05-25 15:36:10 +0200

    change container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 /// test
 export default () => {
     return <>
-        <h1>Hello from container</h1>
+        <h1>Hello from container !!!!!!!!</h1>
         <hr/>
         <MarketingApp/>
     </>
